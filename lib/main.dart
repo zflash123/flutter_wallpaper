3a0b1bfdd231fc63e6b5c2235e121f7ee649781f
Zaed Abdullah <z4ed.thalib123@gmail.com>
2022-05-10 22:15:47 +0700

    + data, categorie_models

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_pretest_navigation/views/home.dart';
+import '/views/home.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
--- lib/model/categories_model.dart
+++ lib/model/categories_model.dart
@@ -1,4 +1,4 @@
-class CategorieModel {
+class CategoriesModel {
   String categorieName;
   String imgUrl;
 }
