706d563616cffa2dc3c625f733c7cecb9c7294c9
Zaed Abdullah <z4ed.thalib123@gmail.com>
2022-05-11 19:15:14 +0700

    complete categorie.dart

---
+++ lib/views/categorie.dart
@@ -0,0 +1,66 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter_wallpaperhub/data/data.dart';
+import 'package:flutter_wallpaperhub/model/wallpaper_model.dart';
+import 'package:flutter_wallpaperhub/widgets/widget.dart';
+
+class CategorieScreen extends StatefulWidget {
+  final String categorie;
+
+  CategorieScreen({@required this.categorie});
+
+  @override
+  _CategorieScreenState createState() => _CategorieScreenState();
+}
+
+class _CategorieScreenState extends State<CategorieScreen> {
+  List<WallpaperModel> photos = new List();
+
+  getCategorieWallpaper() async {
+    await http.get(
+        "https://api.pexels.com/v1/search?query=${widget.categorie}&per_page=30&page=1",
+        headers: {"Authorization": apiKEY}).then((value) {
+      //print(value.body);
+
+      Map<String, dynamic> jsonData = jsonDecode(value.body);
+      jsonData["photos"].forEach((element) {
+        //print(element);
+        WallpaperModel photosModel = new WallpaperModel();
+        photosModel = WallpaperModel.fromMap(element);
+        photos.add(photosModel);
+        //print(photosModel.toString()+ "  "+ photosModel.src.portrait);
+      });
+
+      setState(() {});
+    });
+  }
+
+  @override
+  void initState() {
+    getCategorieWallpaper();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: BrandName(),
+        elevation: 0.0,
+        actions: <Widget>[
+          Container(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: Icon(
+                Icons.add,
+                color: Colors.white,
+              ))
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: wallPaper(photos, context)
+        ,
+      ),
+    );
+  }
+}
